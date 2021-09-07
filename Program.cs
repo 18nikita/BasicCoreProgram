c48f135009b6aeae0dd22d3c843ac7849b3799ec
unknown <rajputnikita5666@gmail.com>
2021-09-07 16:12:15 +0530

    [Nikita] Find Flip coin and there percentage

--- Program.cs
+++ Program.cs
@@ -5,8 +5,41 @@ namespace BasicCoreProgram
     class Program
     {
         static void Main(string[] args)
+
         {
-            Console.WriteLine("Welcome Basic Core Programming");
+            Console.WriteLine("Let's flip a coin 10000 times");
+            Coinflip();
+        }
+        private static int Coinflip()
+
+        {
+            Random Rand = new Random();
+
+            int heads = 0;
+            int tails = 0;
+            int result = 0;
+
+
+            for (int i = 0; i < 10000; i++)
+
+            { 
+                result = Rand.Next(0, 2);
+
+                if (result == 1)
+
+                { 
+                    heads++;
+                }
+
+                else
+
+                {
+                    tails++;
+                }
+            }
+            Console.WriteLine("heads was flipped {0} times", heads);
+            Console.WriteLine("tails was flipped {0} times", tails);
+            return result;
         }
     }
 }
